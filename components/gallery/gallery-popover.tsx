52b9b3f89b7b580e9a4f91e11a7fc0862aeda69d
Yochyo <herodragon25@gmail.com>
2022-04-24 22:45:52 +0200

    optimized gallery for mobile use

--- components/gallery/gallery-popover.tsx
+++ components/gallery/gallery-popover.tsx
@@ -13,7 +13,7 @@ export const GalleryPopover = (props: Props) => {
   return (
     <Transition.Root show={props.open} as={Fragment}>
       <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={props.setOpen}>
-        <div className="flex min-h-screen items-start justify-center px-4 text-center sm:block sm:p-0" onClick={() => props.setOpen(false)}>
+        <div className="flex min-h-screen items-start justify-center text-center sm:block sm:p-0" onClick={() => props.setOpen(false)}>
           {/* background */}
           <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
             <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
@@ -27,7 +27,7 @@ export const GalleryPopover = (props: Props) => {
             leave="ease-in duration-200"
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <div className="relative inline-block h-screen min-w-full p-8">
+            <div className="relative inline-block h-screen min-w-full p-2 sm:p-4 lg:p-8">
               <div className="relative h-full items-center justify-center">
                 <Image src={props.src} layout={'fill'} objectFit={'contain'} alt={'image'} />
                 <div className="flex flex-row-reverse">
