b45e1cd072fb4d60c1fc4e79b85667710dae7a46
Yochyo <herodragon25@gmail.com>
2022-04-29 20:30:45 +0200

    can now control opened image with query param

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,38 +1,52 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Image from 'next/image';
 import { GalleryPopover } from '../components/gallery/gallery-popover';
 import { InferGetStaticPropsType } from 'next';
 import { getGallery } from '../utils/gallery';
+import { useRouter } from 'next/router';
 
 export default function Gallery(props: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [openPopover, setOpenPopover] = useState(false);
-  const [popoverImage, setPopoverImage] = useState<string>('');
+  const [image, setImage] = useState<string | undefined>(undefined);
+  const toPath = (file: string) => `/images/gallery/${file}`;
+
+  const router = useRouter();
+  const [query, setQuery] = useState<string | undefined>();
+
+  const refs: { [key: string]: HTMLDivElement | null } = {};
+
+  const updateQueryParam = (query?: string) => router.push({ href: router.asPath, query: { ...router.query, query } }, undefined, { shallow: true });
+
+  // show image if specified in query
+  useEffect(() => {
+    const imageName = props.gallery.fileNames.find(it => it == query);
+    setImage(imageName ? toPath(imageName) : undefined);
+  }, [props.gallery.fileNames, query]);
+
+  // on initial page load, scroll to selected image. Update query on query change
+  useEffect(() => {
+    const q = router.query.query as string | undefined;
+    if (query == undefined && q != undefined) refs[q]?.scrollIntoView();
+    setQuery(q);
+  }, [router.query]);
 
   return (
     <div className="px-4 py-2 md:px-8 lg:px-16 lg:py-4">
       <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-2 md:grid-cols-3 md:gap-3 lg:grid-cols-4 lg:gap-4">
         {props.gallery.fileNames.map(file => (
-          <div
-            key={file}
-            className="aspect-4-3 relative w-full bg-neutral-200 bg-opacity-40"
-            onClick={() => {
-              setPopoverImage(`/images/gallery/${file}`);
-              setOpenPopover(true);
-            }}>
+          <div key={file} className="aspect-4-3 relative w-full bg-neutral-200 bg-opacity-40" onClick={() => updateQueryParam(file)}>
             <Image src={`/images/gallery/${file}`} layout={'fill'} objectFit={'contain'} alt={file} />
           </div>
         ))}
       </div>
-      <GalleryPopover open={openPopover} setOpen={setOpenPopover} src={popoverImage} />
+      <GalleryPopover onClick={() => updateQueryParam(undefined)} src={image} />
     </div>
   );
 }
 
 export async function getStaticProps({ locale }: any) {
   const gallery = getGallery();
-  console.log(gallery);
   return {
     props: {
       ...(await serverSideTranslations(locale, ['header', 'footer'])),
