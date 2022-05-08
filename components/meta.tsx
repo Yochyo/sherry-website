2a22217259cdcefe227d0888f7706230d0c452d9
Yochyo <herodragon25@gmail.com>
2022-05-08 18:41:10 +0200

    added open graph / twitter card support

---
+++ components/meta.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { PropsWithChildren } from 'react';
+import { useTranslation } from 'next-i18next';
+import Head from 'next/head';
+
+type Props = {
+  url?: string;
+};
+export const Meta = (props: PropsWithChildren<Props>) => {
+  const url = 'https://8be0-2a02-8071-92b9-9400-5df4-8379-4bd8-1791.eu.ngrok.io';
+  const { t } = useTranslation('header');
+
+  return (
+    <Head>
+      <title>{t('meta.title')}</title>
+      <meta name="description" content={t('meta.description')} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={t('meta.title')} />
+      <meta property="og:description" content={t('meta.description')} />
+      <meta property="og:image" content={url + props.url} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@Sherry" />
+      <meta name="twitter:title" content={t('meta.title')} />
+      <meta name="twitter:description" content={t('meta.description')} />
+      <meta name="twitter:image" content={url + props.url} key={'image'} />
+    </Head>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Image from 'next/image';
-import { Footer } from '../components/footer/footer';
-import Navbar from '../components/navbar/navbar';
+import * as React from 'react';
 import { AboutBanner } from '../components/reference/about-banner';
+import { Meta } from '../components/meta';
+import Navbar from '../components/navbar/navbar';
+import { Footer } from '../components/footer/footer';
 import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // @ts-ignore
   let C = Component as any;
+
   return (
     <div className="relative">
+      <Meta url={'/images/meta/twitter-card.jpg'} />
       <Navbar />
       <AboutBanner />
-
       {/* Background desktop */}
       <div className="pointer-events-none hidden opacity-20 md:block">
         <Image src="/background.jpg" layout={'fill'} objectFit={'cover'} objectPosition={'top'} alt="background" />
@@ -22,9 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="pointer-events-none opacity-20 md:hidden">
         <Image src="/background-mobile.jpg" layout={'fill'} objectFit={'cover'} objectPosition={'top'} alt="mobile-background" />
       </div>
-
       <C {...pageProps} />
-
       <Footer />
     </div>
   );
